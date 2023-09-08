0a7ab4adc5435f759f9daf699743bef3aee0e6af
samsonkaleti <kaletishyam@gmail.com>
2023-09-08 23:36:05 +0530

    chaged

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -139,7 +139,7 @@ const Work = () => {
               </span>
               <div className="pt-8 text-center ">
                 {/* eslint-disable-next-line */}
-                <a href='https://bespoke-samsom-worldapp.netlify.app' target="_blank">
+                <a href='https://samsonworldapp.netlify.app' target="_blank">
                 
                   <button
                     className="text-center rounded-lg px-4 py-3 m-2
